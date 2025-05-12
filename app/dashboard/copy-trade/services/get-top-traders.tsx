4d487388a3c0f74c57b8aab0e0c1cd68cd5ce441
nkinnacostly <ayangcostly@gmail.com>
2025-05-12 16:30:54 +0100

    fixes

--- app/dashboard/copy-trade/services/get-top-traders.tsx
+++ app/dashboard/copy-trade/services/get-top-traders.tsx
@@ -2,7 +2,7 @@ import useFetchLevel2 from "@/hooks/useFetchLevel2";
 
 export const useGetTopTraders = () => {
   const { useGetRequest2 } = useFetchLevel2();
-  const url = `/admin/top-traders`;
+  const url = `/top-traders`;
   const reqKey = ["top-traders"];
   const { data, error, isLoading } = useGetRequest2(url, reqKey);
 
