ce9b270950136e822525e497e3f0564088699bf1
nkinnacostly <ayangcostly@gmail.com>
2025-05-28 10:42:55 +0100

    Update API endpoint for fetching single student trade profile

--- app/dashboard/services/get-admin-student-trade-profile.ts
+++ app/dashboard/services/get-admin-student-trade-profile.ts
@@ -2,7 +2,7 @@ import useFetchLevel2 from "@/hooks/useFetchLevel2";
 
 export const useGetSingleStudentTradeProfile = (id: string) => {
   const { useGet } = useFetchLevel2();
-  const url = `/admin/get-single-student-trade-profile/?user_id=${id}`;
+  const url = `/admin/get-single-trade-profile/?user_id=${id}`;
   const reqKey = ["admin-students-trade-profile", id];
   const { data, error, isLoading, isFetching } = useGet(url, reqKey);
 
