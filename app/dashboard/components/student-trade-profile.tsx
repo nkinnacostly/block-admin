5c47377a027e9f24d3708d4888dca1bf46a7ac3c
nkinnacostly <ayangcostly@gmail.com>
2025-07-22 10:00:24 +0100

    Add win rates and final equity fields to StudentTradeProfile component; integrate useGetSingleStudentTraderWinRates hook for data fetching.

--- app/dashboard/components/student-trade-profile.tsx
+++ app/dashboard/components/student-trade-profile.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
-import { useGetSingleStudentTradeProfile } from "../services/get-admin-student-trade-profile";
+import {
+  useGetSingleStudentTradeProfile,
+  useGetSingleStudentTraderWinRates,
+} from "../services/get-admin-student-trade-profile";
 import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
@@ -24,10 +27,15 @@ interface TradeProfile {
 
 function StudentTradeProfile({ id }: { id: string }) {
   const { data, isLoading, isFetching } = useGetSingleStudentTradeProfile(id);
+  const {
+    data: winRates,
+    isLoading: winRatesLoading,
+    isFetching: winRatesFetching,
+  } = useGetSingleStudentTraderWinRates(id);
 
   const tradeProfile = data?.data as TradeProfile;
 
-  if (isLoading || isFetching) {
+  if (isLoading || isFetching || winRatesLoading || winRatesFetching) {
     return (
       <div className="flex items-center justify-center">
         <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
@@ -70,12 +78,24 @@ function StudentTradeProfile({ id }: { id: string }) {
                 <Input defaultValue={tradeProfile.data.investor_password} />
               </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-6 mt-3">
               <div className="space-y-3">
                 <h3 className="text-sm font-medium text-muted-foreground">
                   Server Name
                 </h3>
                 <Input defaultValue={tradeProfile.data.server_name} />
+              </div>
+              <div className="space-y-3">
+                <h3 className="text-sm font-medium text-muted-foreground">
+                  Win Rate
+                </h3>
+                <Input defaultValue={winRates?.win_rate} />
+              </div>
+              <div className="space-y-3">
+                <h3 className="text-sm font-medium text-muted-foreground">
+                  Final Equity
+                </h3>
+                <Input defaultValue={winRates?.equity_growth?.final_equity} />
               </div>
             </div>
           </>
--- app/dashboard/services/get-admin-student-trade-profile.ts
+++ app/dashboard/services/get-admin-student-trade-profile.ts
@@ -1,10 +1,29 @@
 import useFetchLevel2 from "@/hooks/useFetchLevel2";
+import { getSessionStorageItem } from "@/utils/storage";
+import { useQuery } from "@tanstack/react-query";
 
 export const useGetSingleStudentTradeProfile = (id: string) => {
   const { useGet } = useFetchLevel2();
   const url = `/admin/get-single-trade-profile?user_id=${id}`;
   const reqKey = ["admin-students-trade-profile", id];
   const { data, error, isLoading, isFetching } = useGet(url, reqKey);
+
+  return { data, error, isLoading, isFetching };
+};
+
+export const useGetSingleStudentTraderWinRates = (id: string) => {
+  const url = `https://block-traders.com.blocktraders.academy/api/equityGrowth?user_id=${id}`;
+  const token = getSessionStorageItem({ key: "__session" });
+  const reqKey = ["admin-students-trade-win-rates", id];
+  const { data, error, isLoading, isFetching } = useQuery({
+    queryKey: reqKey,
+    queryFn: () =>
+      fetch(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => res.json()),
+  });
 
   return { data, error, isLoading, isFetching };
 };
